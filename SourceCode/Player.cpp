601980a40217a51ff877d0272ddb5fa6deb1202d
ElenaLaura366 <57571504+ElenaLaura366@users.noreply.github.com>
2023-10-26 18:51:23 +0300

    am adaugat variabila membru m_guesed

--- SourceCode/Player.cpp
+++ SourceCode/Player.cpp
@@ -3,13 +3,15 @@
 Player::Player()
 	:m_username("-"),
 	m_score(0),
-	m_isDrawing(0)
+	m_isDrawing(false),
+	m_guesed(false)
 {}
 
 Player::Player(std::string userName)
 	:m_username(userName),
 	m_score(0),
-	m_isDrawing(0)
+	m_isDrawing(false),
+	m_guesed(false)
 {}
 
 uint16_t Player::getScore()
@@ -22,12 +24,33 @@ std::string Player::getName()
 	return this->m_username;
 }
 
+bool Player::getIsDrawing()
+{
+	return this->m_isDrawing;
+}
+
+bool Player::getGuesed()
+{
+	return m_guesed;
+}
+
 void Player::setUserName(std::string userName)
 {
 	this->m_username = userName;
 }
+
 void Player::setIsDrawing(bool isDrawing)
 {
 	this->m_isDrawing = isDrawing;
+}
+
+void Player::setGuesed(bool guesed)
+{
+	this->m_guesed = guesed;
+}
+
+void Player::updateScore(int score)
+{
+	this->m_score = score;
 };
 
--- SourceCode/Player.h
+++ SourceCode/Player.h
@@ -9,11 +9,16 @@ public:
 	Player(std::string userName);
 	uint16_t getScore();
 	std::string getName();
+	bool getIsDrawing();
+	bool getGuesed();
 	void setUserName(std::string userName);
 	void setIsDrawing(bool isDrawing);
+	void setGuesed(bool guesed);
+	void updateScore(int score);
 private:
 	std::string m_username;
 	uint16_t m_score;
 	bool m_isDrawing;
+	bool m_guesed;
 };
 
