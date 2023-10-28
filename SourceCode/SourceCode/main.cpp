db22db79b0835186b24918f6d9dea561d78f7f58
Andreea Angelescu <61116472+AngAnda@users.noreply.github.com>
2023-10-28 15:58:16 +0300

    Deleted unecessary content

--- SourceCode/SourceCode/main.cpp
+++ SourceCode/SourceCode/main.cpp
@@ -4,18 +4,5 @@
 
 int main() {
 
-	WordGenerator wg("Words.txt");
-	for (int i = 0; i < 4; i++)
-	{
-		std::cout << wg.GetWord() <<": ";
-		while (wg.AvailableLetters())
-		{
-			std::cout << wg.GetLetter() << " ";
-		}
-		std::cout << "\n";
-		wg.UpdateWordGenerator();
-	}
-
-
 	return 0;
 }
