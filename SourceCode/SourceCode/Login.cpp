b0fa1e458ea1afab6e7194ee4689b0687cf1a78b
ElenaLaura366 <57571504+ElenaLaura366@users.noreply.github.com>
2023-11-12 12:50:14 +0200

    I added all the getters and setters for the login

--- SourceCode/SourceCode/Login.cpp
+++ SourceCode/SourceCode/Login.cpp
@@ -6,3 +6,23 @@ skribbl::Login::Login() :m_username("-"), m_password("-")
 skribbl::Login::Login(const std::string& username, const std::string& password)
 	: m_username(username), m_password(password) {
 }
+
+std::string skribbl::Login::getUsername() const
+{
+	return m_username;
+}
+
+std::string skribbl::Login::getPassword() const
+{
+	return m_password;
+}
+
+void skribbl::Login::setUsername(const std::string& username)
+{
+	this->m_username = username;
+}
+
+void skribbl::Login::setPassword(const std::string& password)
+{
+	this->m_password = password;
+}
