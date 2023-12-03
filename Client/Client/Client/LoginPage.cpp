471ae49d7ff9fbb2220f7725bc76146c3dc118f2
AlexiaAvramescu <avramescualexia4@gmail.com>
2023-12-03 22:40:57 +0200

    Implemented changetoLogin and changetoRegister in LoginPage.

--- Client/Client/Client/LoginPage.cpp
+++ Client/Client/Client/LoginPage.cpp
@@ -6,12 +6,22 @@ LoginPage::LoginPage(QWidget *parent)
 	, m_windowState(WindowState::LOGIN)
 {
 	ui->setupUi(this);
-	//connect(ui->btnLogin, &QPushButton::clicked, this, &LoginPage::changeToLogin);
-	//connect(ui->btnRegister, &QPushButton::clicked, this, &LoginPage::changeToRegister);
+	connect(ui->btnLogin, &QPushButton::clicked, this, &LoginPage::changeToLogin);
+	connect(ui->btnRegister, &QPushButton::clicked, this, &LoginPage::changeToRegister);
 }
 
 LoginPage::~LoginPage()
 {
 	delete ui;
+}
+
+void LoginPage::changeToLogin()
+{
+	m_windowState = WindowState::LOGIN;
+}
+
+void LoginPage::changeToRegister()
+{
+	m_windowState = WindowState::REGISTER;
 }
 
--- Client/Client/Client/LoginPage.h
+++ Client/Client/Client/LoginPage.h
@@ -7,28 +7,29 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class LoginPageClass; };
 QT_END_NAMESPACE
 
+enum class WindowState {
+	LOGIN,
+	REGISTER
+};
+
 class LoginPage : public QWidget
 {
 	Q_OBJECT
 
 public:
-	LoginPage(QWidget *parent = nullptr);
+	LoginPage(QWidget* parent = nullptr);
 	~LoginPage();
 
+private slots:
+	void changeToLogin();
+	void changeToRegister();
+
+signals:
+	void loginSuccessful();
+
 private:
-	Ui::LoginPageClass *ui;
-	enum class WindowState{
-		LOGIN, 
-		REGISTER
-	};
-
+	Ui::LoginPageClass* ui;
 	WindowState m_windowState;
-//
-//private slots:
-//	void changeToLogin();
-//	void changeToRegister();
 
-//signals:
-//	void loginSuccessful();
 
 };
